b991e3b94475eb010c3f7dd903f7d88ffba55efd
romikps <roman.priscepov@hotmail.com>
2016-05-17 15:51:42 +0300

    First commit

---
+++ app/src/main/java/com/romanpr/seekhide/MainActivity.java
@@ -0,0 +1,130 @@
+package com.romanpr.seekhide;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    LocationManager locationManager;
+    String provider;
+
+    public void changeActivity(View view) {
+
+        // description of an operation
+        Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+        i.putExtra("developersName", "Roman Priscepov");
+        startActivity(i);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // false not to check if provider available
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            Log.i("Location info", "Location achieved!");
+        } else {
+            Log.i("Location info", "No location :(");
+        }
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        final ArrayList<String> friends = new ArrayList<String>();
+        friends.add("Roman K.");
+        friends.add("Roman P.");
+        friends.add("Rozerin");
+        friends.add("Tansel");
+        friends.add("Yekta");
+        friends.add("Beste");
+        friends.add("Ibrahim");
+
+        // link between a set of data and the AdapterView that displays the data
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                                                        friends);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+                i.putExtra("hidersName", friends.get(position));
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.requestLocationUpdates(provider, 3000, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+
+        // telnet localhost 5554
+        // geo fix 11 17
+        Double lat = location.getLatitude();
+        Double lng = location.getLongitude();
+        Float spd = location.getSpeed();
+
+        Log.i("Latitude", lat.toString());
+        Log.i("Longitude", lng.toString());
+        Log.i("Speed", spd.toString());
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void getLocation(View view) {
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        onLocationChanged(location);
+    }
+}
---
+++ app/src/main/java/com/romanpr/seekhide/SecondActivity.java
@@ -0,0 +1,61 @@
+package com.romanpr.seekhide;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public void changeToMain(View view) {
+
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+
+        Intent i = getIntent(); // gets intent that ended us up in this activity, can extract from
+        // Log.i("Developer's name", i.getStringExtra("developersName"));
+
+        welcomeText.setText("Hello, " + i.getStringExtra("hidersName"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
